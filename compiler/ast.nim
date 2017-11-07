1364f63e5feaf9878cbc434b2a4e0fc48261a815
snowlt23 <shsnow23@gmail.com>
2017-11-08 04:42:22 +0900

    add new ast

--- compiler/ast.nim
+++ compiler/ast.nim
@@ -1,5 +1,8 @@
 
+import strutils, sequtils
+
 type
+  FExprError* = object of Exception
   Span* = object
     filename*: string
     line*: int
@@ -8,9 +11,10 @@ type
     internal*: tuple[filename: string, line: int]
   FExprKind* = enum
     fexprIdent
+    fexprAttr
     fexprIntLit
     fexprStrLit
-    fexprLabel
+    fexprInfix
     fexprArray
     fexprList
     fexprBlock
@@ -20,16 +24,96 @@ type
     case kind*: FExprKind
     of fexprIdent:
       ident*: string
+    of fexprAttr:
+      attr*: string
     of fexprIntLit:
       intval*: int64
     of fexprStrLit:
       strval*: string
-    of fexprLabel:
-      labelname*: string
-      labelexpr*: FExpr
-    of fexprArray, fexprList:
-      exprs: seq[FExpr]
-    of fexprBlockCall:
-    of fexprCall:
+    of fexprInfix, fexprArray, fexprList, fexprBlock, fexprCall:
+      sons: seq[FExpr]
 
 const fexprAtoms* = {fexprIdent..fexprStrLit}
+const fexprContainer* = {fexprArray..fexprCall}
+
+proc `$`*(fexpr: FExpr): string
+
+proc error*(fexpr: FExpr, msg: string) =
+  raise newException(FExprError, "$#($#:$#): " % [fexpr.span.filename, $fexpr.span.line, $fexpr.span.linepos] & msg)
+
+template internalSpan*(): Span =
+  const internalname = instantiationInfo().filename
+  const internalline = instantiationInfo().line
+  Span(line: 0, linepos: 0, internal: (internalname, internalline))
+
+proc fident*(span: Span, id: string): FExpr =
+  FExpr(span: span, kind: fexprIdent, ident: id)
+proc fattr*(span: Span, at: string): FExpr =
+  FExpr(span: span, kind: fexprAttr, attr: at)
+proc fintlit*(span: Span, x: int64): FExpr =
+  FExpr(span: span, kind: fexprIntLit, intval: x)
+proc fstrlit*(span: Span, s: string): FExpr =
+  FExpr(span: span, kind: fexprStrLit, strval: s)
+proc finfix*(span: Span, call, left, right: FExpr): FExpr =
+  FExpr(span: span, kind: fexprInfix, sons: @[call, left, right])
+proc farray*(span: Span, sons = newSeq[FExpr]()): FExpr =
+  FExpr(span: span, kind: fexprArray, sons: sons)
+proc flist*(span: Span, sons = newSeq[FExpr]()): FExpr =
+  FExpr(span: span, kind: fexprList, sons: sons)
+proc fblock*(span: Span, sons = newSeq[FExpr]()): FExpr =
+  FExpr(span: span, kind: fexprBlock, sons: sons)
+proc fcall*(span: Span, call: FExpr, args = newSeq[FExpr]()): FExpr =
+  FExpr(span: span, kind: fexprCall, sons: @[call] & args)
+
+proc `[]`*(fexpr: FExpr, i: int): FExpr =
+  if fexpr.kind notin fexprContainer:
+    fexpr.error("$# has not sons" % $fexpr.kind)
+  return fexpr.sons[i]
+proc `[]=`*(fexpr: FExpr, i: int, f: FExpr) =
+  if fexpr.kind notin fexprContainer:
+    fexpr.error("$# has not sons" % $fexpr.kind)
+  fexpr.sons[i] = f
+proc addSon*(fexpr: FExpr, f: FExpr) =
+  if fexpr.kind notin fexprContainer:
+    fexpr.error("$# has not sons" % $fexpr.kind)
+  fexpr.sons.add(f)
+
+iterator items*(fexpr: FExpr): FExpr =
+  if fexpr.kind notin fexprContainer:
+    fexpr.error("$# has not sons" % $fexpr.kind)
+  for son in fexpr.sons:
+    yield(son)
+iterator pairs*(fexpr: FExpr): (int, FExpr) =
+  if fexpr.kind notin fexprContainer:
+    fexpr.error("$# has not sons" % $fexpr.kind)
+  for i, son in fexpr.sons:
+    yield(i, son)
+
+proc len*(fexpr: FExpr): int =
+  if fexpr.kind in fexprContainer:
+    return fexpr.sons.len
+  elif fexpr.kind == fexprInfix:
+    return 2
+  else:
+    return 0
+
+proc `$`*(fexpr: FExpr): string =
+  case fexpr.kind
+  of fexprIdent:
+    fexpr.ident
+  of fexprAttr:
+    ":" & fexpr.attr
+  of fexprIntLit:
+    $fexpr.intval
+  of fexprStrLit:
+    "\"" & fexpr.strval & "\""
+  of fexprInfix:
+    $fexpr.sons[1] & " " & $fexpr.sons[0] & " " & $fexpr.sons[2]
+  of fexprArray:
+    "[" & fexpr.sons.mapIt($it).join(", ") & "]"
+  of fexprList:
+    "(" & fexpr.sons.mapIt($it).join(", ") & ")"
+  of fexprBlock:
+    "{" & fexpr.sons.mapIt($it).join("\n") & "}"
+  of fexprCall:
+    $fexpr.sons[0] & "(" & fexpr.sons[1..^1].mapIt($it).join(", ") & ")"
--- compiler/parser.nim
+++ compiler/parser.nim
@@ -1,2 +1,92 @@
 
-proc 
+import sast
+import strutils
+
+type
+  FParseError* = object of Exception
+  ParserContext* = object
+    filename*: string
+    src*: string
+    line*: int
+    linepos*: int
+    pos*: int
+
+const StartList* = {'(', '['}
+const EndList* = {')', ']'}
+const SpecialSymbols* = {'!', '#', '$', '%', '&', '\'', '*', '+', '-', '.', '/', '<', '=', '>', '?', '^'}
+const SeparateSymbols* = {'(', '[', ')', ']', '{', '}', ',', ' '}
+
+proc newParserContext*(filename: string, src: string): ParserContext =
+  result.filename = filename
+  result.src = src
+  result.line = 1
+  result.linepos = 1
+  result.pos = 0
+proc curchar*(context: ParserContext): char =
+  return context.src[context.pos]
+proc isEOF*(context: ParserContext): bool =
+  context.curchar == 0x1a.char or context.curchar == '\0'
+proc isNewline*(context: ParserContext): bool =
+  let lf = 0x0a.char
+  let cr = 0x0d.char
+  if context.curchar == lf:
+    return true
+  elif context.src[context.pos] == cr and context.src[context.pos+1] == lf:
+    return true
+  else:
+    return false
+proc newlineLen*(context: ParserContext): int =
+  let lf = 0x0a.char
+  if context.curchar == lf:
+    return 1
+  else:
+    return 2
+
+proc isSeparateSymbol*(context: ParserContext): bool =
+  if context.curchar in SeparateSymbols or context.isNewline:
+    return true
+  else:
+    return false
+proc inc*(context: var ParserContext) =
+  context.linepos += 1
+  context.pos += 1
+proc skipSpaces*(context: var ParserContext) =
+  while true:
+    if context.curchar == ' ':
+      context.inc
+    elif context.isNewLine:
+      context.line += 1
+      context.linepos = 1
+      context.pos += context.newlineLen()
+    elif context.curchar == ';':
+      context.inc
+      while true:
+        if context.isNewline:
+          context.line += 1
+          context.linepos = 1
+          context.pos += context.newlineLen()
+          break
+        context.inc
+    else:
+      break
+proc span*(context: ParserContext): Span =
+  result.filename = context.filename
+  result.line = context.line
+  result.linepos = context.linepos
+  result.pos = context.pos
+
+# TODO:
+proc parseFExpr*(context: var ParserContext): SExpr = discard
+
+proc parseSExpr*(filename: string, src: string): SExpr =
+  var context = newParserContext(filename, src)
+  return parseFExpr(context)
+proc parseToplevel*(filename: string, src: string): seq[SExpr] =
+  var context = newParserContext(filename, src)
+  result = @[]
+  while true:
+    let ret = parseFExpr(context)
+    if ret == nil:
+      break
+    else:
+      result.add(ret)
